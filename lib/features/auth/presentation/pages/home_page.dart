5a9de226b7e439091b0063537b68f7d9c5548611
Tolgonai <tolgonai.mamytova.bj@gmail.com>
2024-01-02 23:06:47 +0300

    do

---
+++ lib/features/auth/presentation/pages/home_page.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<GridMenuItem> gridMenuItem = [];
+
+  @override
+  void initState() {
+    super.initState();
+    gridMenuItem = [
+      GridMenuItem(
+          title: '', image: 'assets/images/category.png', onTap: () {}),
+      GridMenuItem(
+          title: '', image: 'assets/images/category1.png', onTap: () {}),
+      GridMenuItem(
+          title: '', image: 'assets/images/category2.png', onTap: () {}),
+      GridMenuItem(
+          title: '', image: 'assets/images/category3.png', onTap: () {}),
+      GridMenuItem(
+          title: '', image: 'assets/images/category4.png', onTap: () {}),
+      GridMenuItem(
+        title: '',
+        image: 'assets/images/category5.png',
+        onTap: () {},
+      )
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Эко Маркет',
+          style: TextStyle(
+            color: Color.fromARGB(255, 4, 2, 2),
+            fontSize: 24,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 18,
+          left: 16,
+          right: 16,
+        ),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 11.0,
+            crossAxisSpacing: 11.0,
+          ),
+          itemCount: gridMenuItem.length,
+          itemBuilder: (context, index) {
+            String svgPath = gridMenuItem[index].image;
+            return SizedBox(
+                child: DecoratedBox(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                image: DecorationImage(
+                    image: AssetImage(svgPath), fit: BoxFit.cover),
+              ),
+              child: Text(
+                gridMenuItem[index].title,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
+              ),
+            ));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class GridMenuItem {
+  GridMenuItem({
+    required this.title,
+    required this.image,
+    required this.onTap,
+  });
+
+  final String title;
+  final String image;
+  final VoidCallback onTap;
+}
+//       bottomNavigationBar: SizedBox(
+//         child: BottomNavigationBar(
+//           currentIndex: _selectedIndex,
+//           onTap: (index) {
+//             routerScreen(index: index);
+//           },
+//           items: const <BottomNavigationBarItem>[
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.home),
+//               label: 'Главная',
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.shopping_bag),
+//               label: 'Корзина',
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.history),
+//               label: 'История',
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.info_outline_rounded),
+//               label: 'Инфо',
+//             ),
+//           ],
+//           selectedItemColor: Color(0xff75db1b),
+//           unselectedItemColor: Colors.grey,
+//           showUnselectedLabels: true,
+//         ),
+//       ),
+//     );
+//   }
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+// }
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:eco_market/app/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
