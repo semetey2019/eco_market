65df6df6bdaca58ae7578a111c0034caf26e4c3f
Tolgonai <tolgonai.mamytova.bj@gmail.com>
2024-01-29 08:18:09 +0300

    done

--- lib/features/main/presentation/pages/product/product_page.dart
+++ lib/features/main/presentation/pages/product/product_page.dart
@@ -270,105 +270,108 @@ class _ProductPageState extends State<ProductPage> {
             padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
             child: Wrap(
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.asset(
-                      'assets/images/orange.png',
-                      fit: BoxFit.cover,
-                    ),
-                    const SizedBox(
-                      height: 12,
-                    ),
-                    const Text(
-                      'Апельсины сладкий пакистанский',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
+                SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.asset(
+                        'assets/images/orange.png',
+                        fit: BoxFit.cover,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    const Text(
-                      '56 c шт',
-                      style: TextStyle(
-                        color: Color(0xFF75DB1B),
-                        fontSize: 24,
-                        fontWeight: FontWeight.w700,
+                      const SizedBox(
+                        height: 12,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    text(
-                      'Cочный плод яблони, который употребляется в пищу в свежем и запеченном виде, служит сырьём в кулинарии и для приготовления напитков.',
-                    ),
-                    const SizedBox(height: 24),
-                    isAdded
-                        ? Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text(
-                                '112 с',
-                                style: TextStyle(
-                                  color: Color(0xFF1E1E1E),
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w700,
+                      const Text(
+                        'Апельсины сладкий пакистанский',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        '56 c шт',
+                        style: TextStyle(
+                          color: Color(0xFF75DB1B),
+                          fontSize: 24,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      text(
+                        'Cочный плод яблони, который употребляется в пищу в свежем и запеченном виде, служит сырьём в кулинарии и для приготовления напитков.',
+                      ),
+                      const SizedBox(height: 24),
+                      isAdded
+                          ? Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  '112 с',
+                                  style: TextStyle(
+                                    color: Color(0xFF1E1E1E),
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.w700,
+                                  ),
                                 ),
-                              ),
-                              Row(
-                                children: [
-                                  IconButtonWidget(
-                                    icon: Icons.remove,
-                                    onTap: () {
-                                      setState(
-                                        () {
-                                          item.decrementCounter();
-                                        },
-                                      );
-                                    },
-                                  ),
-                                  const SizedBox(
-                                    width: 44,
-                                  ),
-                                  Text(
-                                    item.getCounter().toString(),
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    width: 44,
-                                  ),
-                                  IconButtonWidget(
-                                      icon: Icons.add,
+                                Row(
+                                  children: [
+                                    IconButtonWidget(
+                                      icon: Icons.remove,
                                       onTap: () {
-                                        setState(() {
-                                          item.incrementCounter();
-                                        });
-                                      })
-                                ],
-                              ),
-                            ],
-                          )
-                        : CustomButtomWidget(
-                            text: 'Добавить',
-                            onPressed: () {
-                              setState(
-                                () {
-                                  isAdded = true;
-                                },
-                              );
-                            },
-                            height: 54,
-                          ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                  ],
+                                        setState(
+                                          () {
+                                            item.decrementCounter();
+                                          },
+                                        );
+                                      },
+                                    ),
+                                    const SizedBox(
+                                      width: 44,
+                                    ),
+                                    Text(
+                                      item.getCounter().toString(),
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: 44,
+                                    ),
+                                    IconButtonWidget(
+                                        icon: Icons.add,
+                                        onTap: () {
+                                          setState(() {
+                                            item.incrementCounter();
+                                          });
+                                        })
+                                  ],
+                                ),
+                              ],
+                            )
+                          : CustomButtomWidget(
+                              text: 'Добавить',
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    isAdded = true;
+                                  },
+                                );
+                              },
+                              height: 54,
+                            ),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
